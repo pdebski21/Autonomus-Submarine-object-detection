bd4fa805413550047e4fb30be80d99ad82ea0b75
Piotr Debski <ppdebski@interia.eu>
2022-04-17 19:38:13 +0200

    color detection tool and board detection with white hsv color detection and postprocessing
    Signed-off-by: Piotr Debski <ppdebski@interia.eu>

---
+++ color_detection_tool.py
@@ -0,0 +1,106 @@
+#finding hsv range of target object(pen)
+import cv2
+import numpy as np
+import time
+# A required callback method that goes into the trackbar function.
+def nothing(x):
+    pass
+
+# Initializing the webcam feed.
+#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("samples/board_video/2016_0922_050010_016.MP4")
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+out = cv2.VideoWriter("/home/piotrdebski/Pulpit/Robocik/board_detection/samples/research_log/color_detection.mp4", fourcc, 20.0, (640, 480))
+#cap.set(3,1280)
+#cap.set(4,720)
+frame_counter = 0
+
+erosion_size = 0
+max_elem = 2
+max_kernel_size = 21
+
+# Create a window named trackbars.
+cv2.namedWindow("Trackbars")
+
+# Now create 6 trackbars that will control the lower and upper range of 
+# H,S and V channels. The Arguments are like this: Name of trackbar, 
+# window name, range,callback function. For Hue the range is 0-179 and
+# for S,V its 0-255.
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+while cap.isOpened():
+    
+    # Start reading the webcam feed frame by frame.
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # Flip the frame horizontally (Not required)
+    frame_counter += 1
+    #If the last frame is reached, reset the capture and the frame_counter
+    if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        frame_counter = 0 #Or whatever as long as it is the same as next line
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        #out.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    frame = cv2.flip( frame, 1 ) 
+    
+    # Convert the BGR image to HSV image.
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # Get the new values of the trackbar in real time as the user changes 
+    # them
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+    
+ 
+    # Set the lower and upper HSV range according to the value selected
+    # by the trackbar
+    lower_range = np.array([l_h, l_s, l_v])
+    upper_range = np.array([u_h, u_s, u_v])
+    
+    # Filter the image and get the binary mask, where white represents 
+    # your target color
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+ 
+    # You can also visualize the real part of the target color (Optional)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    
+    # Converting the binary mask to 3 channel image, this is just so 
+    # we can stack it with the others
+    mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+   
+    # stack the mask, orginal frame and the filtered result
+    stacked = np.hstack((mask_3,frame,res))
+   
+    # Show this stacked frame at 40% of the size.
+    cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.4,fy=0.4))
+
+    out.write(mask_3)
+  
+    # If the user presses ESC then exit the program
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+    # If the user presses `s` then print this array.
+    if key == ord('s'):
+        
+        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+        print(thearray)
+        
+        # Also save this array as penval.npy
+        np.save('hsv_value',thearray)
+        break
+    
+# Release the camera & destroy the windows.    
+cap.release()
+out.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,29 +1,102 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from skimage import exposure
 
-def captureFrame():
-    cap = cv.VideoCapture(0)
+def captureFrame(filename=0):
+    cap = cv.VideoCapture(filename)
     count = 0
     while cap.isOpened():
         ret, frame = cap.read()
+        detectColor(frame)
         frame = detectCorners(frame)
-        cv.imshow("window-name", frame)
+        cv.imshow("capture", frame)
         # cv.imwrite("frame%d.jpg" % count, frame)
         count = count + 1
         if cv.waitKey(10) & 0xFF == ord("q"):
             break
     cap.release()
 
+def detectColor(frame):
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+   
+    # White color
+    low_white = np.array([84,200,100])
+    high_white = np.array([93,255,144])
+    white_mask = cv.inRange(hsv_frame, low_white, high_white)
+    white = cv.bitwise_and(frame, frame, mask=white_mask)
+    # Red color
+    low_red = np.array([161, 155, 84])
+    high_red = np.array([172, 255, 255])
+    red_mask = cv.inRange(hsv_frame, low_red, high_red)
+    red = cv.bitwise_and(frame, frame, mask=red_mask)
+    # Blue color
+    low_blue = np.array([94, 80, 2])
+    high_blue = np.array([126, 255, 255])
+    blue_mask = cv.inRange(hsv_frame, low_blue, high_blue)
+    blue = cv.bitwise_and(frame, frame, mask=blue_mask)
+    # Green color
+    low_green = np.array([25, 52, 72])
+    high_green = np.array([102, 255, 255])
+    green_mask = cv.inRange(hsv_frame, low_green, high_green)
+    green = cv.bitwise_and(frame, frame, mask=green_mask)
+    # Every color except white
+    low = np.array([0, 42, 0])
+    high = np.array([179, 255, 255])
+    mask = cv.inRange(hsv_frame, low, high)
+    result = cv.bitwise_and(frame, frame, mask=mask)
+ 
+    cv.imshow("White", white)
+    cv.imshow("Red", red)
+    cv.imshow("Blue", blue)
+    cv.imshow("Green", green)
+    cv.imshow("Result", result)
+
+    # corners detection    
+    corners = detectCorners(white)
+    cv.imshow("Corners", corners)
+
+    # erode with cross kernel
+    eroded = erosion(white, 3, 2)
+    cv.imshow("Eroded", eroded)
+
+    # dilate with rect kernel
+    dilatated = dilatation(eroded, 5, 0)    
+    cv.imshow("Dilatated", dilatated)
+    
 
 def detectCorners(frame):
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     dst = cv.cornerHarris(gray, 2, 3, 0.04)
-    dst = cv.dilate(dst, None)
+    #dst = cv.dilate(dst, None)
     frame[dst > 0.01 * dst.max()] = [0, 255, 0]
     return frame
 
+
+def morph_shape(val):
+    if val == 0:
+        return cv.MORPH_RECT
+    elif val == 1:
+        return cv.MORPH_CROSS
+    elif val == 2:
+        return cv.MORPH_ELLIPSE
+
+
+def erosion(frame, erosion_size, erosion_shape):
+    erosion_shape = morph_shape(erosion_shape)
+    element = cv.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1),
+                                       (erosion_size, erosion_size))
+    erosion_res = cv.erode(frame, element)
+    return erosion_res
+
+
+def dilatation(frame, dilatation_size, dilatation_shape):
+    dilatation_shape = morph_shape(dilatation_shape)
+    element = cv.getStructuringElement(dilatation_shape, (2 * dilatation_size + 1, 2 * dilatation_size + 1),
+                                       (dilatation_size, dilatation_size))
+    dilatation_res = cv.dilate(frame, element)
+    return dilatation_res
 
 def FeatureMatching(template, frame, minMatchCnt=10):
     sift = cv.SIFT_create()
@@ -57,12 +130,12 @@ def FeatureMatching(template, frame, minMatchCnt=10):
             [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]
         ).reshape(-1, 1, 2)
         dst = cv.perspectiveTransform(pts, M)
-        frame = cv.polylines(frame, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+        #frame = cv.polylines(frame, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
     else:
         # better use for logging
         print(
             "Not enough matches are found - {}/{}".format(
-                len(good), minMatchCount
+                len(good), minMatchCnt
             )
         )
         matchesMask = None
@@ -79,47 +152,61 @@ def FeatureMatching(template, frame, minMatchCnt=10):
     displayFrame(frame_matches, '3')
     
 
-def histogramMatching(template, frame):
-    multi = True if src.shape[-1] > 1 else False
+def matchHistograms(template, frame):
+    multi = True if template.shape[-1] > 1 and frame.shape[-1] > 1 else False
     matched = exposure.match_histograms(template, frame, multichannel=multi)
+    final_frame = cv.hconcat((template, frame, matched))
+    #displayFrame(matched, 'matched_histograms')
     return matched
 
 
-
-
 def compareHistograms(template, frame):
-    template_hist = cv.calcHist([template],[0],None,[256],[0,256])
-    frame_hist = cv.calcHist([template],[0],None,[256],[0,256])
-    color = ('b','g','r')
-    for i,col in enumerate(color):
-        template_histr = cv.calcHist([template],[i],None,[256],[0,256])
-        frame_histr = cv.calcHist([frame],[i],None,[256],[0,256])
-        plt.plot(template_histr,color = col)
-        plt.plot(frame_histr,color = col)
-        plt.xlim([0,256])
+    mask = np.zeros(frame.shape[:2], np.uint8)
+    mask[10:750, 10:370] = 255
+    masked_frame = cv.bitwise_and(frame,frame,mask = mask)
+    # Calculate histogram with mask and without mask
+    # Check third argument for mask
+    hist_full = cv.calcHist([frame],[0],None,[256],[0,256])
+    hist_mask = cv.calcHist([frame],[0],mask,[256],[0,256])
+    plt.subplot(221), plt.imshow(frame, 'gray')
+    plt.subplot(222), plt.imshow(mask,'gray')
+    plt.subplot(223), plt.imshow(masked_frame, 'gray')
+    plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
+    plt.xlim([0,256])
     plt.show()
 
 
 def displayFrame(frame, windowName):
+    cv.resizeWindow(windowName, 300, 400)
     cv.imshow(windowName, frame)
     if cv.waitKey(0) & 0xFF == ord('q'):
         cv.destroyAllWindows()
 
 
 def main():
-    # captureFrame()
+    captureFrame("samples/board_video/2016_0922_050010_016.MP4")
+    """
+    captureFrame()
     template = cv.imread(
         "/home/piotrdebski/Pulpit/Robocik/board_detection/samples/board_images/object_gangster.png"
     )
     frame = cv.imread(
         "/home/piotrdebski/Pulpit/Robocik/board_detection/samples/board_images/objects.png"
+        #"/home/piotrdebski/Pulpit/Robocik/board_detection/samples/board_images/pedestraints.png"
     )
+    frame_grey = cv.imread(
+        "/home/piotrdebski/Pulpit/Robocik/board_detection/samples/board_images/objects_grey.png"
+    ) 
+
+ 
     FeatureMatching(template, frame)
     template_corners = detectCorners(template)
     frame_corners = detectCorners(frame)
     displayFrame(template_corners, "1")
     displayFrame(frame_corners, "2")
-
+    """
+    #compareHistograms(template, frame)
+    #matched = matchHistograms(frame, frame_grey)
 
 if __name__ == "__main__":
     main()
